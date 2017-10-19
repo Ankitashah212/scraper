f0f305796b705ecd86fa162b27a2c29052349474
Kita <ankitashah212@gmail.com>
2017-10-19 19:32:01 -0400

    adding files for mongoose

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
-/* Students: Using the tools and techniques you learned so far,
- * you will scrape a website of your choice, then place the data
- * in a MongoDB database. Be sure to make the database and collection
- * before running this exercise.
 
- * Consult the assignment files from earlier in class
- * if you need a refresher on Cheerio. */
-
-// Dependencies
 var express = require("express");
 var mongojs = require("mongojs");
-// Require request and cheerio. This makes the scraping possible
 var request = require("request");
 var cheerio = require("cheerio");
 
+var results = [];
 // Initialize Express
 var app = express();
 app.use(express.static("public"));
@@ -38,38 +30,38 @@ app.get("/scrape", function (req, res) {
   request("http://www.nytimes.com", function (error, response, html) {
 
     var $ = cheerio.load(html);
-    // An empty array to save the data that we'll scrape
-    var results = [];
-
-    // Select each element in the HTML body from which you want information.
-    // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-    // but be sure to visit the package's npm page to see how it works
+    var counter = 0;
     $("h2.story-heading").each(function (i, element) {
 
       var link = $(element).children().attr("href");
       var title = $(element).children().text();
-
+      title.replace("\n", "");
       // Save these results in an object that we'll push into the results array we defined earlier
-      results.push({
-        title: title,
-        link: link
-      });
-      db.scrapedData.insert({
-        title: title,
-        link: link
-      });
+      if (title && link) {
+        results.push({
+          counter: counter,
+          title: title,
+          link: link
+        });
+        counter++;
+        /*      db.scrapedData.insert({
+                title: title,
+                link: link
+              });*/
+      }
     });
 
+
     // Log the results once you've looped through each of the elements found with cheerio
-    console.log(results);
+    //console.log(results);
     res.send(results);
   });
 
 
 });
 
-app.get("/all", function (req, res) {
-
+app.post("/save/:id", function (req, res) {
+  console.log(req.params.id);
 });
 
 // Listen on port 3000
